92a90fc22120196fac8c7ec663c51c979bac43e4
Treenawat Manomairat <treenawatmanomairat@Treenawats-MacBook-Pro.local>
2022-01-26 12:38:45 -0600

    Edit unit test

---
+++ ParseLang.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * parse : a class to parse csv files into a string list
+ * 
+ * @param args : args not used, user input is gathered via java.util.Scanner
+ * 
+ *             use: user enters es/en to choose which file to parse. The file is
+ *             then parsed and stored in an array list.
+ */
+public class ParseLang {
+
+	public static ArrayList<String> parseLang(String lang) throws FileNotFoundException, IOException {
+
+		ArrayList<String> letters = new ArrayList<String>();// Create an ArrayList object
+
+		// tryen to create a buffered reader for a file reader
+		try (BufferedReader csv = new BufferedReader(new FileReader(lang + ".csv"))) {
+
+			// creating a temp String row for the row data
+			String row;
+
+			// creating a string array to hold the row data
+			String[] data = null;
+
+			// Read each line parse it into array split by comma
+			while ((row = csv.readLine()) != null) {
+				// System.out.println(row);
+				data = row.split(",");
+				for (String s : data) {
+					letters.add(s);
+				}
+			}
+
+		}
+		return letters;
+	}
+
+	public static void main(String[] args) throws IOException {
+		parseLang("en");
+	}
+
+}
--- parseTest.java
+++ parseTest.java
@@ -1,4 +1,4 @@
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -11,38 +11,39 @@ class parseTest {
 
 	@Test
 	void test() throws FileNotFoundException, IOException {
-		String[] en = new String[] {"A","a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G","g","H", "h",
-				"I","i","J","j","K","k","L","l","M","m", "N","n","O","o","P","p","Q","q","R","r","S"
-				,"s","T","t","U","u","V","v","W","w","X","x","Y","y","Z","z"};
-		
-		String[] es = new String[] {"A", "a", "B" ,"b", "C" , "c", "D" , "d", "E", "e", "F", "f", "G", "g",
-				"H" ,"h", "I", "i", "J", "j", "K", "k", "L", "l", "LL", "ll",
-				"M" , "m", "N", "n","ñ", "O", "o", "P", "p", "Q", "q", "R", "r", "RR", "rr", "S", "s", "T", "t", "U" ,"u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z"};
-		
-		ArrayList<String> actual1 = parse.parse_lang("en");
-		ArrayList<String> actual2 = parse.parse_lang("es");
+		// Characters use for comparison
+		String[] en = { "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J",
+				"j", "K", "k", "L", "l", "M", "m", "N", "n", "O", "o", "P", "p", "Q", "q", "R", "r", "S", "s", "T", "t",
+				"U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z" };
+
+		String[] es = { "A", "a", "B", "b", "C", "c", "D", "d", "E", "e", "F", "f", "G", "g", "H", "h", "I", "i", "J",
+				"j", "K", "k", "L", "l", "LL", "ll", "M", "m", "N", "n", "ñ", "O", "o", "P", "p", "Q", "q", "R", "r",
+				"RR", "rr", "S", "s", "T", "t", "U", "u", "V", "v", "W", "w", "X", "x", "Y", "y", "Z", "z" };
+
+		// Create arraylist for en and es
+		ArrayList<String> actual1 = ParseLang.parseLang("en");
+		ArrayList<String> actual2 = ParseLang.parseLang("es");
 		Random random = new Random();
-		
-	
-		
+
+		// Test
 		assertEquals(en[0], actual1.get(0));
-		assertEquals(en[en.length-1], actual1.get(actual1.size()-1));
-		
+		assertEquals(en[en.length - 1], actual1.get(actual1.size() - 1));
+
 		assertEquals(es[0], actual2.get(0));
-		assertEquals(es[es.length-1], actual2.get(actual2.size()-1));
-		
+		assertEquals(es[es.length - 1], actual2.get(actual2.size() - 1));
+
 		int r = random.nextInt(51);
 		assertEquals(en[r], actual1.get(r));
 		assertEquals(es[r], actual2.get(r));
-		
+
 		r = random.nextInt(51);
 		assertEquals(en[r], actual1.get(r));
 		assertEquals(es[r], actual2.get(r));
-		
+
 		r = random.nextInt(51);
 		assertEquals(en[r], actual1.get(r));
 		assertEquals(es[r], actual2.get(r));
-		
+
 	}
-	
+
 }
